e5ac4e353407ee38013ba485e493b52ed763eae1
Mariya Barska <moiweer@gmail.com>
2022-06-22 18:39:37 +0300

    конспект

--- js/masiv_metods.js
+++ js/masiv_metods.js
@@ -74,3 +74,287 @@
 
 // processCall("Манго", takeCall, activateAnsweringMachine);
 // processCall("Полі", takeCall, leaveHoloMessage);
+
+
+
+// імперативний підхід
+// Імперативний підхід
+// const numbers = [1, 2, 3, 4, 5];
+// const filteredNumbers = [];
+
+// for (let i = 0; i < numbers.length; i += 1) {
+//   if (numbers[i] > 3) {
+//     filteredNumbers.push(numbers[i]);
+//   }
+// }
+
+// console.log(filteredNumbers); // [4, 5]
+
+
+// Декларативний підхід
+// const numbers = [1, 2, 3, 4, 5];
+// const filteredNumbers = numbers.filter(value => value > 3);
+// console.log(filteredNumbers); // [4, 5]
+
+// Перебираючі методи масиву
+// Метод Map()
+// синтаксис:
+
+// массив.map((element, index, array) => {
+//   // Тіло колбек-функції
+// });
+
+
+
+// const planets = ["Земля", "Марс", "Венера", "Юпітер"];
+
+// const planetsInUpperCase = planets.map(planet => planet.toUpperCase());
+// console.log(planetsInUpperCase); // ['ЗЕМЛЯ', 'МАРС', 'ВЕНЕРА', 'ЮПІТЕР']
+
+// const planetsInLowerCase = planets.map(planet => planet.toLowerCase());
+// console.log(planetsInLowerCase); // ['земля', 'марс', 'венера', 'юпітер']
+
+// // Оригінальний масив не змінився
+// console.log(planets); // ['Земля', 'Марс', 'Венера', 'Юпітер']
+
+
+
+
+// отримання масиву імен студентів з масиву об'єктів
+
+
+// const students = [
+//   { name: "Манго", score: 83 },
+//   { name: "Полі", score: 59 },
+//   { name: "Аякс", score: 37 },
+//   { name: "Ківі", score: 94 },
+//   { name: "Х'юстон", score: 64 },
+// ];
+
+// const names = students.map(student => student.name);
+// console.log(names); // ['Манго', 'Полі', 'Аякс', 'Ківі', 'Х'юстон']
+
+
+
+// // метод flatMap()
+// const students = [
+//   { name: "Манго", courses: ["математика", "фізика"] },
+//   { name: "Полі", courses: ["інформатика", "математика"] },
+//   { name: "Ківі", courses: ["фізика", "біологія"] },
+// ];
+
+// students.map(student => student.courses);
+// // [['математика', 'фізика'], ['інформатика', 'математика'], ['фізика', 'біологія']]
+
+// students.flatMap(student => student.courses);
+// // ['математика', 'фізика', 'інформатика', 'математика', 'фізика', 'біологія'];
+
+
+
+// Метод filter(callback) використовується для 
+// єдиної операції - фільтрації масиву,
+// тобто, коли необхідно вибрати більше одного елемента
+//  з колекції за певним критерієм.
+
+// масив.filter((element, index, array) => {
+//   // Тіло колбек-функції
+// });
+
+
+
+// const values = [51, -3, 27, 21, -68, 42, -37];
+
+// const positiveValues = values.filter(value => value >= 0);
+// console.log(positiveValues); // [51, 27, 21, 42]
+
+// const negativeValues = values.filter(value => value < 0);
+// console.log(negativeValues); // [-3, -68, -37]
+
+// const bigValues = values.filter(value => value > 1000);
+// console.log(bigValues); // []
+
+// // Оригінальний масив не змінився
+// console.log(values); // [51, -3, 27, 21, -68, 42, -37]
+
+
+
+// Наприклад, у нас є масив студентів з балами за тест. 
+// Необхідно відфільтрувати кращих(бал вище 80), гірших(бал нижче 50) 
+// і середніх студентів(бал від 50 до 80).
+
+
+
+// const LOW_SCORE = 50;
+// const HIGH_SCORE = 80;
+// const students = [
+//   { name: "Манго", score: 83 },
+//   { name: "Полі", score: 59 },
+//   { name: "Аякс", score: 37 },
+//   { name: "Ківі", score: 94 },
+//   { name: "Х'юстон", score: 64 },
+// ];
+
+// const best = students.filter(student => student.score >= HIGH_SCORE);
+// console.log(best); // Масив об'єктів з іменами Манго і Ківі
+
+// const worst = students.filter(student => student.score < LOW_SCORE);
+// console.log(worst); // Масив з одним об'єктом Аякс
+
+// // В колбек-функції зручно деструктуризувати властивості об'єкта
+// const average = students.filter(
+//   ({ score }) => score >= LOW_SCORE && score < HIGH_SCORE
+// );
+// console.log(average); // Масив об'єктів з іменами Полі і Х'юстон
+
+
+
+// Метод find()
+// синтаксис
+// масив.find((element, index, array) => {
+  // Тіло колбек-функції
+// });
+
+
+// const colorPickerOptions = [
+//   { label: "red", color: "#F44336" },
+//   { label: "green", color: "#4CAF50" },
+//   { label: "blue", color: "#2196F3" },
+//   { label: "pink", color: "#E91E63" },
+//   { label: "indigo", color: "#3F51B5" },
+// ];
+
+// colorPickerOptions.find(option => option.label === "blue"); // { label: 'blue', color: '#2196F3' }
+// colorPickerOptions.find(option => option.label === "pink"); // { label: 'pink', color: '#E91E63' }
+// colorPickerOptions.find(option => option.label === "white"); // undefined
+
+
+
+
+// Метод findIndex()
+// Метод findIndex(callback) - це сучасна заміна методу indexOf().Дозволяє виконувати пошук за складнішими умовами, ніж просто рівність. 
+// Використовується як для пошуку у масиві примітивів, так і в масиві об'єктів.
+// const colorPickerOptions = [
+//   { label: "red", color: "#F44336" },
+//   { label: "green", color: "#4CAF50" },
+//   { label: "blue", color: "#2196F3" },
+//   { label: "pink", color: "#E91E63" },
+//   { label: "indigo", color: "#3F51B5" },
+// ];
+
+// colorPickerOptions.findIndex(option => option.label === "blue"); // 2
+// colorPickerOptions.findIndex(option => option.label === "pink"); // 3
+// colorPickerOptions.findIndex(option => option.label === "white"); // -1
+
+
+
+// Методи every() і some()
+// Перевіряє, чи проходять всі елементи масиву тест колбек-функції. Повертає true або false
+
+
+// масив.every((element, index, array) => {
+//   // Тіло колбек-функції
+// });
+
+
+// // Усі елементи більші або дорівнюють нулю? - так
+// [1, 2, 3, 4, 5].every(value => value >= 0); // true
+
+// // Усі елементи більші або дорівнюють нулю? - ні
+// [1, 2, 3, -10, 4, 5].every(value => value >= 0); // false
+
+// Метод some()
+// Перевіряє, чи проходить хоча б один елемент масиву тест колбек-функції. Повертає true або false
+
+// масив.some((element, index, array) => {
+//   // Тіло колбек-функції
+// });
+
+
+
+
+
+
+// Під час роботи з масивом об'єктів перевіряється значення певної їх властивості. 
+// Наприклад, у нас є масив об'єктів фруктів,
+//  необхідно дізнатися, чи є фрукти в наявності 
+// та чи є в наявності хоча б якісь фрукти, тобто більше 0 штук
+
+
+// const fruits = [
+//   { name: "apples", amount: 100 },
+//   { name: "bananas", amount: 0 },
+//   { name: "grapes", amount: 50 },
+// ];
+
+// // every поверне true тільки, якщо усіх фруктів буде більше 0 штук
+// const allAvailable = fruits.every(fruit => fruit.amount > 0); // false
+
+// // some поверне true, якщо хоча б одного фрукту буде більше 0 штук
+// const anyAvailable = fruits.some(fruits => fruits.amount > 0); // true
+
+
+
+
+// Метод reduce()
+
+// масив.reduce((previousValue, element, index, array) => {
+//   // Тіло колбек-функції
+// }, initialValue);
+
+
+
+// підрахунку суми елементів масиву.
+// const total = [2, 7, 3, 14, 6].reduce((previousValue, number) => {
+//   return previousValue + number;
+// }, 0);
+
+// console.log(total); // 32
+
+
+
+
+
+
+// Під час роботи з масивом об'єктів виконується редукування за значенням 
+// певної властивості. 
+//     Наприклад, у нас є масив студентів з балами за тест.
+// Необхідно отримати середній бал
+// const students = [
+//   { name: "Манго", score: 83 },
+//   { name: "Полі", score: 59 },
+//   { name: "Аякс", score: 37 },
+//   { name: "Ківі", score: 94 },
+//   { name: "Х'юстон", score: 64 },
+// ];
+
+// // Назва акумулятора може бути довільною, це просто параметр функції
+// const totalScore = students.reduce((total, student) => {
+//   return total + student.score;
+// }, 0);
+
+// const averageScore = totalScore / students.length;
+
+
+// задача: з масиву постів твіттера окремого
+//  користувача необхідно порахувати суму усіх лайків
+// const tweets = [
+//   { id: "000", likes: 5, tags: ["js", "nodejs"] },
+//   { id: "001", likes: 2, tags: ["html", "css"] },
+//   { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+//   { id: "003", likes: 8, tags: ["css", "react"] },
+//   { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
+// ];
+
+// // Пройдемо по всіх елементах колекції і додамо значення властивості likes
+// // до акумулятора, початкове значення якого вкажемо 0.
+// const likes = tweets.reduce((totalLikes, tweet) => totalLikes + tweet.likes, 0);
+
+// console.log(likes); // 32
+
+// // Мабуть, підрахунок лайків - не одиночна операція, тому напишемо функцію
+// // для підрахунку лайків з колекції
+// const countLikes = tweets => {
+//   return tweets.reduce((totalLikes, tweet) => totalLikes + tweet.likes, 0);
+// };
+
+// console.log(countLikes(tweets)); // 32
